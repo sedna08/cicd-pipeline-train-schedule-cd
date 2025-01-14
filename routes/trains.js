b928796cf153a1309f4d5a48c1d969b5cdece033
Mitcham Andes Ochia <109507870+sedna08@users.noreply.github.com>
2025-01-14 16:24:06 +0800

    Update trains.js

--- routes/trains.js
+++ routes/trains.js
@@ -3,9 +3,7 @@ var router = express.Router();
 var low = require('lowdb')
 var FileSync = require('lowdb/adapters/FileSync')
 
-var path = require('path');
-var adapter = new FileSync(path.join(__dirname, 'data', 'trains.json'));
-// var adapter = new FileSync('data/trains.json')
+var adapter = new FileSync('data/trains.json')
 var db = low(adapter)
 
 console.log('Current working directory:', process.cwd());
